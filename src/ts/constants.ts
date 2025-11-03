eadb8179211f444874ecc05e8467e4c7be6de82d
Edilson Luna <edilsonluna117@gmail.com>
2025-11-03 10:58:24 -0500

    Adicion de archivos landing page

---
+++ src/ts/constants.ts
@@ -0,0 +1,5 @@
+export const rutaWhatsappSoporte: string = "https://wa.me/3102642209?text=Hola%20Buenos%20d%C3%ADas%2C%20quiero%20realizar%20una%20cotizaci%C3%B3n%20de%20sus%20servicios.";
+export const rutaInstagram:string = "https://www.instagram.com/delthac1seguridad/?hl=es";
+export const rutaLinkedIn:string = "https://co.linkedin.com/company/delthac-1-seguridad";
+export const rutaFacebook:string = "https://www.facebook.com/delthac1seguridad/";
+export const rutaYoutube:string = "https://www.youtube.com/@delthac1_seguridad/featured";
---
+++ src/ts/env.d.ts
@@ -0,0 +1,1 @@
+declare const ScrollReveal: any;
